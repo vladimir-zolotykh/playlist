dd2765058c7a77e44d03f3c12582668ccff9c861
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-05-04 20:35:13 +0300

    Bugfix, refactor

--- blondie.pls
+++ blondie.pls
@@ -2,7 +2,6 @@
 File1=Blondie\Atomic\01-Atomic.ogg
 Title1=Blondie - Atomic
 Length1=230
-...
 File18=Blondie\Atomic\18-I'm Gonna Love You Too.ogg
 Title18=Blondie - I'm Gonna Love You Too
 Length18=-1
--- playlist.py
+++ playlist.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+from typing import Union
 import re
 
 INI_HEADER = re.compile(r"^\[[^]]+\]$")
 KEY_VALUE_RE = re.compile(r"^(?P<key>\w+)\s*=\s*(?P<value>.*)$")
+VOC_TYPE = dict[str, Union[str, int]]  # vocabulary
 
 
-def parse(lowercase_keys):
-    key_values = {}
+def parse(file, vocabulary: VOC_TYPE, lowercase_keys=False) -> VOC_TYPE:
     for lino, line in enumerate(file, start=1):
         line = line.strip()
         if not line or line.startswith("#"):
@@ -18,8 +19,16 @@ def parse(lowercase_keys):
             key = key_value.group("key")
             if lowercase_keys:
                 key = key.lower()
-                key_values[key] = key_value.group("value")
-            else:
-                ini_header = INI_HEADER.match(line)
-                if not ini_header:
-                    print("Failed to parse line {0}: {1}".format(lino, line))
+            vocabulary[key] = key_value.group("value")
+        else:
+            ini_header = INI_HEADER.match(line)
+            if not ini_header:
+                print("Failed to parse line {0}: {1}".format(lino, line))
+    return vocabulary
+
+
+blondie_voc: VOC_TYPE = {}
+if __name__ == "__main__":
+    with open("blondie.pls") as f:
+        parse(f, blondie_voc)
+    print(blondie_voc)
