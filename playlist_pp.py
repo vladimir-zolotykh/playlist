8a742bba062e8bb4c839143442bda6d42fbf37d1
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-05-05 18:16:44 +0300

    Use set_result_name()

--- playlist_pp.py
+++ playlist_pp.py
@@ -7,8 +7,8 @@ import pyparsing as pp
 
 def bnf() -> pp.core.ParserElement:
     ini_header = pp.Combine(pp.Literal("[") + pp.Regex(r"[^]]+") + pp.Literal("]"))
-    value = pp.Regex(r".+")
-    key = pp.Word(pp.alphas + pp.alphanums)
+    key = pp.Word(pp.alphas + pp.alphanums + "_").set_results_name("key")
+    value = pp.restOfLine.set_results_name("value")
     key_value = key + pp.Suppress("=") + value
     blank = pp.Regex(r"^$")
     comment = pp.Regex(r"#.*")
@@ -21,6 +21,6 @@ if __name__ == "__main__":
     with open("blondie_pp.pls", "r") as f:
         for line in f:
             res = bnf().parse_string(line[:-1])
-            if len(res) == 2:
-                pairs[res[0]] = res[1]
+            if "key" in res:
+                pairs[res["key"]] = res["value"]
     print(pairs)
