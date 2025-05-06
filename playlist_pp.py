da8226fd1e24fe5f0713038060571b23f8d45a1c
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-05-06 09:37:08 +0300

    Add parse_string(), extend doctest
    Duplicated blondie_pp.pls as a string (argument of parse_string)

--- playlist_pp.py
+++ playlist_pp.py
@@ -7,8 +7,25 @@
 'Length1': '230', 'File18': "Blondie/Atomic/18-I'm Gonna Love You Too.ogg", \
 'Title18': "Blondie - I'm Gonna Love You Too", 'Length18': '-1', \
 'NumberOfEntries': '18', 'Version': '2'}
+>>> parse_string("[playlist]\\n\
+File1=Blondie/Atomic/01-Atomic.ogg\\n\
+Title1=Blondie - Atomic\\n\
+Length1=230\\n\
+# A comment\\n\
+\\n\
+File18=Blondie/Atomic/18-I'm Gonna Love You Too.ogg\\n\
+Title18=Blondie - I'm Gonna Love You Too\\n\
+Length18=-1\\n\
+NumberOfEntries=18\\n\
+Version=2\\n\
+")
+{'File1': 'Blondie/Atomic/01-Atomic.ogg', 'Title1': 'Blondie - Atomic', \
+'Length1': '230', 'File18': "Blondie/Atomic/18-I'm Gonna Love You Too.ogg", \
+'Title18': "Blondie - I'm Gonna Love You Too", 'Length18': '-1', \
+'NumberOfEntries': '18', 'Version': '2'}
 """
 from typing import Any
+import io
 import pyparsing as pp
 
 PLS_PAIRS = dict[str, Any]
@@ -26,14 +43,24 @@ def bnf() -> pp.core.ParserElement:
     return line
 
 
-def parse_file(filename="blondie_pp.pls") -> PLS_PAIRS:
+def parse_string(str_literal) -> PLS_PAIRS:
+    so = io.StringIO(str_literal)
+    return parse_file_obj(so)
+
+
+def parse_file_obj(fo) -> PLS_PAIRS:
     pairs: PLS_PAIRS = {}
-    with open(filename, "r") as f:
-        for line in f:
-            res = bnf().parse_string(line[:-1])
-            if "key" in res:
-                pairs[res["key"]] = res["value"]
+    for line in fo:
+        res = bnf().parse_string(line[:-1])
+        if "key" in res:
+            pairs[res["key"]] = res["value"]
     return pairs
+
+
+def parse_file(filename="blondie_pp.pls") -> PLS_PAIRS:
+
+    with open(filename, "r") as f:
+        return parse_file_obj(f)
 
 
 if __name__ == "__main__":
